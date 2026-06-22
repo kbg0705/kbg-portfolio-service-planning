e1ba4ee1571d8fa60e196dea7abcde3d0187624a
kbg0705 <kbg0705@ewha.ac.kr>
2026-06-21 23:16:04 -0700

    Load portfolio application

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import PortfolioApp from './PortfolioApp';
 import './styles/global.css';
-import './styles/ux-polish.css';
 
 const redirectPath = new URLSearchParams(window.location.search).get('redirect');
-
 if (redirectPath) {
-  const basePath = window.location.pathname.startsWith('/miricanvas-junior-pm-portfolio')
-    ? '/miricanvas-junior-pm-portfolio'
-    : '';
+  const basePath = window.location.pathname.startsWith('/miricanvas-junior-pm-portfolio') ? '/miricanvas-junior-pm-portfolio' : '';
   window.history.replaceState(null, '', `${basePath}${redirectPath}`);
 }
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(<StrictMode><PortfolioApp /></StrictMode>);
